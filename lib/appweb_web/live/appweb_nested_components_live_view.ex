5de93a8cc6430ac37a80b3363210ab3b09a25e99
zgbjgg <zgbjgg@gmail.com>
2019-10-16 15:52:13 -0500

    add nested components example with version 0.1.9

---
+++ lib/appweb_web/live/appweb_nested_components_live_view.ex
@@ -0,0 +1,27 @@
+defmodule AppWeb.NestedComponentsLiveView do
+  use Quetzal.LiveView,
+    handler: __MODULE__,
+    callbacks: [:update_output_div_and_pie_graph]
+
+  @impl Quetzal.LiveView
+  def components(_session) do
+    {"MYAPP", [{Quetzal.Form, [id: "myform", name: "myform",
+       children: [{Quetzal.InputText, [id: "mytext", value: "", name: "mytext"]},
+         {Quetzal.InputText, [id: "mytext2", value: "", name: "mytext2"]}]
+     ]},
+     {Quetzal.Div, [id: "mydiv", style: "", children: ""]},
+     {Quetzal.Div, [id: "mydiv2", style: "", children: "If you write inside the inputs above the pie graph and div output will change."]},
+     {Quetzal.Div, [id: "mydivwithchildren", children: [
+       {Quetzal.Graph, [id: "mypieg"], [type: "pie", labels: ["Red", "Blue"], values: [10, 20]]}]
+     ]}]}
+  end
+
+  def update_output_div_and_pie_graph("myform", _target, fields) do
+    # This updates the div and the pie graph at the same time
+    value = fields["mytext"]
+    value2 = fields["mytext2"]
+    newvalues = for _n <- 1..3, do: :rand.uniform(100)
+    [mydiv: [children: "You've entered #{value} value in the first input and #{value2} in the second one"],
+     mypieg: [labels: ["Black", "White", "Gray"], values: newvalues]]
+  end
+end
