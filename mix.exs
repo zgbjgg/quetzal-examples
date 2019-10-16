82510e08c62b5afd326068fdb3248266e6d327d4
zgbjgg <zgbjgg@gmail.com>
2019-10-16 15:52:26 -0500

    upgrade to v0.1.9

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule AppWeb.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:quetzal, "~> 0.1.8"}
+      {:quetzal, "~> 0.1.9"}
     ]
   end
 end
